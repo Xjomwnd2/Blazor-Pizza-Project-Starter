ad02425a312ecfd3b2e0e096ff30e0518a3038e1
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-24 05:41:50 +0300

    commit

--- PizzaApp/Models/Pizza.cs
+++ PizzaApp/Models/Pizza.cs
@@ -1,11 +1,10 @@
 namespace PizzaApp.Models
 {
-public class Pizza
-{
-public int Id { get; set; }
-public string Name { get; set; } = string.Empty;
-public string Description { get; set; } = string.Empty;
-public decimal Price { get; set; }
-public bool IsVegetarian { get; set; }
-}
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
 }
