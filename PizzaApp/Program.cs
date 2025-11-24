3c5e3140902e484b2ecf4977b4e8feee1ad63ed9
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-24 05:13:58 +0300

    commit

--- PizzaApp/Program.cs
+++ PizzaApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Services;
 using PizzaApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,23 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+// SQLite connection (file in project root)
+var conn = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=pizza.db";
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(conn));
+
+
+// Register pizza service
+builder.Services.AddScoped<IPizzaService, PizzaService>();
+
+
 var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+app.UseExceptionHandler("/Error");
+app.UseHsts();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
